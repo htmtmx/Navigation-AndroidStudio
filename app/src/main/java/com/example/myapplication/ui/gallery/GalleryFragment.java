94c5868df7994560a283cab80cae224b0aa36c77
htmtmx <cesar.hpp96@hotmail.com>
2022-04-20 18:47:56 -0500

    Navigation -  Android Studio, JAVA

---
+++ app/src/main/java/com/example/myapplication/ui/gallery/GalleryFragment.java
@@ -0,0 +1,53 @@
+package com.example.myapplication.ui.gallery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.FullScreenActivity;
+import com.example.myapplication.ImageAdapter;
+import com.example.myapplication.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+    private FragmentGalleryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        GalleryViewModel galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
+
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textGallery;
+        GridView gridView = binding.gridView;
+        gridView.setAdapter(new ImageAdapter(this.getContext()));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getContext(), FullScreenActivity.class);
+                intent.putExtra("id", i);
+                startActivity(intent);
+            }
+        });
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
